865bd43a9893921a13ca101ab4524c07328d06d1
fernandorohr <fernando.felipe.flesch@gmail.com>
2020-11-18 09:02:37 -0300

    department class created

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.Department;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "book");
+        System.out.println(department);
+    }
+}
