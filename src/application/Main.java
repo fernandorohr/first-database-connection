b3e2009bcfdd3f5c84cbe5e249f44e797e73e9c1
fernandorohr <fernando.felipe.flesch@gmail.com>
2020-11-18 14:11:31 -0300

    implemented delete method

--- src/application/Main.java
+++ src/application/Main.java
@@ -27,15 +27,19 @@ public class Main {
         List<Seller> allSellers = sellerDao.findAll();
         allSellers.forEach(System.out::println);
 
-        System.out.println("\n-------INSERT A NEW SELLER-------");
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.0, new Department(2, null));
-        sellerDao.insert(newSeller);
-        System.out.println("New seller ID: " + newSeller.getId());
+//        System.out.println("\n-------INSERT A NEW SELLER-------");
+//        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.0, new Department(2, null));
+//        sellerDao.insert(newSeller);
+//        System.out.println("New seller ID: " + newSeller.getId());
 
         System.out.println("\n-------UPDATE A SELLER-------");
         seller = sellerDao.findById(1);
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n-------FIND SELLER BY DEPARTMENT-------");
+        sellerDao.deleteById(10);
+        System.out.println("Deletion completed");
     }
 }
